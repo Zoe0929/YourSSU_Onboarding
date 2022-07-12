fd1e00d67bc66b743cc981664f855a4db8a1af4b
Zoe <mam07065@naver.com>
2022-07-12 20:52:15 +0900

    MusicPlayer - 화면 구성

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 지희의 MAC on 2022/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
