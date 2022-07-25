c7a764594057d6126117d109f3c3bee5ffeda988
Zoe <mam07065@naver.com>
2022-07-25 23:40:39 +0900

    회원가입 화면 - 화면 전환

---
+++ MySignUp/MySignUp/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  MySignUp
+//
+//  Created by 지희의 MAC on 2022/07/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func popToprev(){
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    @IBAction func dismissModal(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MySignUp/MySignUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySignUp
+//
+//  Created by 지희의 MAC on 2022/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
