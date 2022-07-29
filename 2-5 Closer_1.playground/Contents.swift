1c3f9481a71b6cbb4207829fc526406b22aa82f7
Zoe <mam07065@naver.com>
2022-07-29 23:55:15 +0900

    2-18 클로저 심화

--- 2-5 Closer_1.playground/Contents.swift
+++ 2-5 Closer_1.playground/Contents.swift
@@ -34,3 +34,54 @@ calculated = calculate(a: 50, b: 10, method: { (left: Int, right: Int) -> Int in
     return left * right
 })
 print(calculated)
+
+
+//후행 클로저
+var result: Int
+result = calculate(a: 10, b: 10) { (left: Int, right: Int) -> Int in
+    return left + right
+}
+
+print(result) // 20
+
+//반환 타입 생략
+result = calculate(a: 10, b: 10, method: { (left: Int, right: Int) in
+    return left + right
+})
+
+print(result) // 20
+
+// 후행클로저와 함께 사용할 수도 있습니다
+result = calculate(a: 10, b: 10) { (left: Int, right: Int) in
+    return left + right
+}
+
+print(result) // 20
+
+//단축 인자이름
+result = calculate(a: 10, b: 10, method: {
+    return $0 + $1
+})
+
+print(result) // 20
+
+
+// 당연히 후행 클로저와 함께 사용할 수 있습니다
+result = calculate(a: 10, b: 10) {
+    return $0 + $1
+}
+
+print(result) // 20
+
+//암시적 반환
+result = calculate(a: 10, b: 10) {
+    $0 + $1
+}
+
+print(result) // 20
+
+// 간결하게 한 줄로 표현해 줄 수도 있습니다
+result = calculate(a: 10, b: 10) { $0 + $1 }
+
+print(result) // 20
+
