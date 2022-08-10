cee420e17eae6e7d16105e2c9d7dd90f9229371f
Zoe <mam07065@naver.com>
2022-08-10 19:59:07 +0900

    키보드 올리기, 버튼 활성화

--- SignUp/SignUp/SecondViewController.swift
+++ SignUp/SignUp/SecondViewController.swift
@@ -21,8 +21,11 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.imageView.isUserInteractionEnabled = true
           //제쳐스 추가
         self.imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.imageViewTapped)))
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown), name: UIResponder.keyboardWillHideNotification, object: nil)
         introduceView.layer.cornerRadius = 15
         enableBtn(isOn: false)
+        
     }
     
     //이미지 뷰 탭 이용 이미지 선택
@@ -67,15 +70,19 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
     }
     
-    @IBAction func textViewDidEndEditing(_ sender: UITextView){
+    func textViewDidEndEditing(_ sender: UITextView!){
+        print("\(idTextField.text!) \(passwordTextField.text!) \(passwordCheckTextField.text!)")
+        let placehold:String = "자기소개 입력"
         if sender.text.isEmpty{
-            sender.text = ""
+            sender.text=placehold
             self.enableBtn(isOn: false)
-        }else if idTextField.text?.isEmpty == false {
-            if let password = passwordTextField.text, let passwordCheck = passwordCheckTextField.text, password == passwordCheck, imageView.image != nil {
-                NextButton.isUserInteractionEnabled = true
+        }else if sender.text == placehold{
+            sender.text=""
+            self.enableBtn(isOn: false)
+        }else if textfieldisFill(), passwordCheck(passwordTextField, passwordCheckTextField) {
+            NextButton.isUserInteractionEnabled = true
+            self.enableBtn(isOn: true)
             }
-        }
     }
     
     @IBAction func textFieldDidEndEditing(_ textField: UITextField){
@@ -83,7 +90,9 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
             self.enableBtn(isOn: false)
         }
         else if textField.text != ""{
-            self.enableBtn(isOn: true)
+            if textfieldisFill(), passwordCheck(passwordTextField, passwordCheckTextField){
+                self.enableBtn(isOn: true)
+            }
         }
     }
     
@@ -96,6 +105,37 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.view.endEditing(true)
     }
 
+    //키보드 올라 올 때 뷰 올리기
+    @objc func keyboardUp(notification:NSNotification) {
+        if let keyboardFrame:NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+           let keyboardRectangle = keyboardFrame.cgRectValue
+       
+            UIView.animate(
+                withDuration: 0.5
+                , animations: {
+                    self.view.transform = CGAffineTransform(translationX: 0, y: -(keyboardRectangle.height/9))
+                }
+            )
+        }
+    }
+    @objc func keyboardDown() {
+        self.view.transform = .identity
+    }
+    
+    func passwordCheck(_ pw: UITextField, _ pwcheck:UITextField)->Bool{
+        if pw.text == pwcheck.text {
+            return true
+        }
+        else {return false}
+    }
+    func textfieldisFill()->Bool{
+        if idTextField.text?.isEmpty == false , passwordTextField.text?.isEmpty == false, passwordCheckTextField.text?.isEmpty==false {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
     /*
     // MARK: - Navigation
